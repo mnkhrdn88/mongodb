8d0ea075473c6b2d433aba9e94ec5aa41dbd4079
mnkhrdn88 <batmnkhrdn@gmail.com>
2025-01-30 20:25:25 +0800

    task1 done

---
+++ index.js
@@ -0,0 +1,46 @@
+// const express = require("express");
+
+import express from "express";
+import { MongoClient } from "mongodb";
+const app = express();
+const port = 3000;
+
+const url =
+  "mongodb+srv://batmnkhrdn:dYlNifuyakxr9jF0@cluster0.c706r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(url);
+
+const db = await client.db("sample_mflix");
+const collection = await db.collection("movies");
+
+app.get("/", async (req, res) => {
+  const movies = await collection
+    .find({})
+    .project({ title: 1, year: 1, _id: 0 })
+    .limit(5)
+    .toArray();
+
+  res.send(movies);
+});
+
+app.get("/task1", async (req, res) => {
+  const { title } = req.query;
+
+  const movie = await collection.findOne({
+    title: { $eq: title },
+  });
+
+  res.send(movie);
+});
+
+app.listen(port, async () => {
+  await client
+    .connect()
+    .then(() => {
+      console.log("connected To DB");
+    })
+    .catch((err) => console.log("err", err));
+
+  console.log(`Example app listening on port ${port}`);
+});
+
+//dYlNifuyakxr9jF0
